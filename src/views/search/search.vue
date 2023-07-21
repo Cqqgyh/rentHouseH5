5c1570050dded8b5c0b9e507e7dffd1a445ee4d8
cqq <945327638@qq.com>
2023-07-21 17:05:24 +0800

    feat: 找房页面功能完成

--- src/views/search/search.vue
+++ src/views/search/search.vue
@@ -1,6 +1,21 @@
 <template>
   <SearchBar :confirm-callback="confirmCallback"></SearchBar>
-  <div class="main-container">主要内容</div>
+
+  <div class="main-container">
+    <PullDownRefreshContainer
+      :request="getRoomListHandler"
+      ref="pullDownRefreshContainerRef"
+      class="min-h-[70vh]"
+    >
+      <template v-if="roomList?.length">
+        <RoomCard
+          v-for="item in roomList"
+          :key="item.id"
+          :data="item"
+        ></RoomCard>
+      </template>
+    </PullDownRefreshContainer>
+  </div>
 </template>
 
 <script setup lang="ts" name="Search">
@@ -8,26 +23,19 @@ import SearchBar from "@/components/SearchBar/SearchBar.vue";
 import type { RoomInterface, RoomListQueryInterface } from "@/api/search/types";
 import { ref } from "vue";
 import { getRoomList } from "@/api/search";
-// 分页信息
-const pageInfo = ref({
-  current: 1,
-  size: 10
-});
+import PullDownRefreshContainer from "@/components/PullDownRefreshContainer/PullDownRefreshContainer.vue";
+import type { ReqPage } from "@/api/types";
+import RoomCard from "@/components/RoomCard/RoomCard.vue";
 // 搜索参数
 const searchParams = ref<Partial<RoomListQueryInterface>>({});
+const pullDownRefreshContainerRef =
+  ref<InstanceType<typeof PullDownRefreshContainer>>();
 // 房间列表
 const roomList = ref<RoomInterface[]>([]);
 // 点击查找，搜索条件
 function confirmCallback(params: RoomListQueryInterface) {
-  // 重置分页信息
-  pageInfo.value = {
-    current: 1,
-    size: 10
-  };
   //    收集参数，调用接口
-  let targetObi = {
-    ...pageInfo.value
-  };
+  let targetObi = {};
   //   去除空值
   for (let key in params) {
     if ((params as any)[key] !== "") {
@@ -35,12 +43,20 @@ function confirmCallback(params: RoomListQueryInterface) {
     }
   }
   searchParams.value = targetObi;
-  getRoomListHandler();
+  pullDownRefreshContainerRef.value?.onRefresh();
 }
 // 分页获取房间的信息
-async function getRoomListHandler() {
+async function getRoomListHandler(pageInfo: ReqPage) {
   //   调用接口
-  const { data } = await getRoomList(searchParams.value);
-  roomList.value = data.records;
+  const { data } = await getRoomList({ ...searchParams.value, ...pageInfo });
+  console.log('pageInfo.current',pageInfo.current);
+  if (pageInfo.current === 1) {
+    roomList.value = data.records;
+  } else {
+    roomList.value = [...roomList.value, ...data.records];
+  }
+  pullDownRefreshContainerRef.value?.setFinished(
+    roomList.value.length >= data.total
+  );
 }
 </script>
